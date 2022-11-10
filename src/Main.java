ff92803ebe88c2c68d4876852d9d4c6328798c23
Danny Mousa <57102573+F4KER-X@users.noreply.github.com>
2022-11-10 07:37:01 -0500

    u

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 import java.io.FileReader;
 
 public class Main {
-    public static void main(String[] args) throws FileInvalidException {
+    public static void main(String[] args) {
         System.out.println("Welcome to BibilographyFactory!\n");
         final int NUMBER_OF_FILES = 10;
         final String INPUT_EXT = ".bib";
@@ -19,33 +19,126 @@ public class Main {
         if (!outDir.exists()) {
             outDir.mkdirs();
         }
-//        //open files
-//        openFiles(scanArray, NUMBER_OF_FILES, INPUT_FILE_NAME, INPUT_EXT);
-//        //create files
-//        createFiles(pwArray, scanArray, NUMBER_OF_FILES, OUTPUT_FILE_NAME, OUTPUT_EXT);
+        //open files
+        openFiles(scanArray, NUMBER_OF_FILES, INPUT_FILE_NAME, INPUT_EXT);
+        //create files
+        createFiles(pwArray, scanArray, NUMBER_OF_FILES, OUTPUT_FILE_NAME, OUTPUT_EXT);
 
-        try {
-            String[] test = new String[0];
-            int counter = 1;
-            Scanner scan = new Scanner(new FileInputStream("Latex1.bib"));
-//            String fileToString = fileToString(scan);
-//            String[] filetoStringArray = fileToString.split("@ARTICLE");
+
+
+
+
+        int counter = 0;
+        for (int t = 0; t < scanArray.length; t++) {
+            String error = "";
+            String author = "";
+            String journal = "";
+            String year = "";
+            String title = "";
+            String volume = "";
+            String[] records = new String[0];
+            counter = 0;
+            try {
+                //Scanner scan = new Scanner(new FileInputStream("Latex3.bib"));
+                String fileToString = fileToString(scanArray[t]);
+                String[] fileToStringArray = fileToString.trim().split("@ARTICLE");
+                System.out.println(fileToStringArray.length);
+                records = new String[fileToStringArray.length];
+                for (String f : fileToStringArray) {
+                    String[] singleArticle = f.split(",");
+                    for (int i = 0; i < singleArticle.length; i++) {
+                        String[] info = singleArticle[i].split("=");
+                        if (info.length >= 2) {
+                            String filed = info[0].trim();
+                            if (info[1].trim().substring(1, info[1].length() - 1).isEmpty()) {
+                                error = info[0].trim();
+                                throw new FileInvalidException();
+                            }
+                            switch (filed) {
+                                case "author":
+                                    author = info[1].substring(1, info[1].length() - 1).trim();
+                                    break;
+                                case "journal":
+                                    journal = info[1].substring(1, info[1].length() - 1).trim();
+                                    break;
+                                case "volume":
+                                    volume = "vol." + info[1].substring(1, info[1].length() - 1).trim();
+                                    break;
+                                case "year":
+                                    year = info[1].substring(1, info[1].length() - 1).trim();
+                                    break;
+
+
+                            }
+
+                        }
+                    }
+
+                    //System.out.println(author);
+                    records[counter] = author + " " + journal + " " + volume + " " + year;
+                    counter++;
+
+                }
+
+                for (String r : records) {
+                    System.out.println(r);
+                }
+                System.out.println("ended file " + (t + 1));
+                System.out.println();
+
+
 //            for (String s : filetoStringArray) {
 //                test = s.split(",");
-//                //System.out.println(counter + test[0]);
+//                System.out.println(s);
 //                counter++;
 //            }
+//            String[] test1 = filetoStringArray[1].split(",");
+//            String[] testing = {"{X. Li and M. A. Salehi and M. Bayoumi and N. F. Tzeng and R. Buyya}",
+//                    "{IEEE Transactions on Parallel and Distributed Systems}",
+//                    "{Cost-Efficient and Robust On-Demand Video Transcoding Using Heterogeneous Cloud Services}",
+//                    "{2018}",
+//                    "{}",
+//                    "{3}",
+//                    "{556-571}",
+//                    "{Cloud computing;Delays;Quality of service;Robustness;Spatial resolution;Streaming media;Transcoding;Cloud services;On-demand video transcoding;QoS-aware scheduling;heterogeneous VM provisioning}",
+//                    "{10.1109/TPDS.2017.2766069}",
+//                    "{1045-9219}",
+//                    "{March}"};
+//
+//            for (String t : testing) {
+//                System.out.println(t);
+//                if (t.substring(1, t.length() - 1).isEmpty()) {
+//                    System.out.println("YEEEEES");
+//
+//                }
+//            }
+//            for (String t : test1) {
+//                //System.out.println(t);
+//                String[] tt = t.split("=");
+//                if (tt.length >= 2) {
+//                    for (int i = 1; i < tt.length; i++) {
+//                        System.out.println(tt[i].trim());
+//                        if (tt[i].substring(1, tt[i].length() - 1).isEmpty()) {
+//                            System.out.println("YEEEEES");
+//
+//                        }
+//                    }
+//                }
+//
+//            }
+
+                // System.out.println(filetoStringArray[1]);
 //            System.out.println(filetoStringArray[4]);
-            while (scan.hasNextLine()){
-                System.out.println(scan.nextLine());
-            }
-            // System.out.println(fileToString);
+//            while (scan.hasNextLine()){
+//                System.out.println(scan.nextLine());
+//            }
+                // System.out.println(fileToString);
 
 
-           // int counter = 0;
-            int articleNumber = 0;
+                // int counter = 0;
+                int articleNumber = 0;
 
-            //String line = scan.nextLine();
+                //String line = scan.nextLine();
 //            while (scan.hasNextLine()) {
 //
 //                String line = scan.nextLine();
@@ -73,9 +166,15 @@ public class Main {
 //
 //
 //            }
-            //System.out.println(counter);
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+                //System.out.println(counter);
+
+            } catch (FileInvalidException e) {
+                System.out.println(e.getMessage() + "file " + t);
+                System.out.println("error location " + error);
+                records = new String[0];
+            }
+
+
         }
     }
 
@@ -146,7 +245,9 @@ public class Main {
     public static String fileToString(Scanner scan) {
         String fileAsString = "";
         while (scan.hasNextLine()) {
-            fileAsString += scan.nextLine();
+            String tempLine = scan.nextLine();
+            if (!tempLine.isEmpty())
+                fileAsString += tempLine;
         }
 
         //check
